b713496d393092058abb2853eb8ef52624a78c6b
Andrew Sturm <luther.sturm@gmail.com>
2022-08-22 18:39:40 -0500

    Add 'configparser'
    ConfigParser allows you to remove the end user from editing the script.

--- ping.py
+++ ping.py
@@ -3,10 +3,18 @@ import csv
 import platform
 import subprocess
 import slackweb
+import configparser
+
+config = configparser.ConfigParser()
+
+try:
+    config.read('config.ini')
+except FileNotFoundError:
+    print("Error: 'config.ini' does not exist.")
 
 # Set up slack notifier -- Change this to match your webhook address
 slack = slackweb.Slack(
-    url="https://hooks.slack.com/services/<slackwebhookaddress>")
+    url=f"https://hooks.slack.com/services/{config['slack']['webhook']}")
 
 downhosts = []
 
@@ -16,8 +24,8 @@ def ping_ip(ip):
     try:
         output = subprocess.check_output(
             "ping -{} 1 {}".format(
-                'n' if platform.system().lower() == "windows" else 'c', ip), 
-            shell=True, 
+                'n' if platform.system().lower() == "windows" else 'c', ip),
+            shell=True,
             universal_newlines=True)
         if 'unreachable' in output:
             return False
@@ -32,7 +40,7 @@ while 1 == 1:
     # Set to number of seconds between checks.
     time.sleep(60)
     # Make sure to set path correctly.
-    with open('/home/pi/python/monitoring/hosts.csv') as csvfile:
+    with open(config['files']['hosts']) as csvfile:
         read_csv = csv.reader(csvfile, delimiter=',')
         for row in read_csv:
             host = row[0]
@@ -40,13 +48,13 @@ while 1 == 1:
 
             if ping_ip(ip):
                 if host in downhosts:
-                    # Feel free to change the username and emoji on this 
+                    # Feel free to change the username and emoji on this
                     # section, and the other slack.notify section.
                     slack.notify(text=time.strftime("%m/%d/%Y - %H:%M:%S")
-                                 + " [***] " + ip + " - " 
-                                 + host + " appears to be back up", 
-                                 username="Arthur-Dent", 
-                                 icon_emoji=":rocket:")
+                                 + " [***] " + ip + " - "
+                                 + host + " appears to be back up",
+                                 username=config['slack']['username'],
+                                 icon_emoji=config['slack']['emoji'])
                     downhosts.remove(host)
             else:
                 if host in downhosts:
@@ -57,7 +65,7 @@ while 1 == 1:
                     message = "Host offline " + host + ip
                     slack.notify(text=time.strftime("%m/%d/%Y - %H:%M:%S")
                                  + " [***] " + ip + " - "
-                                 + host + " appears down", 
-                                 username="Arthur-Dent",
-                                 icon_emoji=":rocket:")
+                                 + host + " appears down",
+                                 username=config['slack']['username'],
+                                 icon_emoji=config['slack']['emoji'])
                     downhosts = downhosts + [host]
